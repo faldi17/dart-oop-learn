fa669c0131baad874dd463d8c303c57d2a98cc13
Faldi <faldiansyahk@gmail.com>
2025-10-05 06:32:54 +0700

    make constructor with car

---
+++ bin/constructor.dart
@@ -0,0 +1,28 @@
+void main() {
+  Car car = Car("Lamborgini", "Red");
+  print(car.road(120, "Tol Serpong"));
+  // car.road();
+}
+
+class Car {
+  String brand = "Ferrari";
+  final wheel = 4;
+  String? color;
+
+  Car(String paramBrand, String paramColor) {
+    brand = paramBrand;
+    color = paramColor;
+  }
+
+  // method road to call in main
+  String road(int speed, String location) {
+    if (color == "Red") {
+      color = "Merah";
+    }
+    return "Mobil $brand $color melaju dengan kecepatan $speed km/jam di $location. Ngeeeng... ngeeeng...";
+  }
+
+  // void road() {
+  //   print("Ngeeeng... ngeeeng...");
+  // }
+}
