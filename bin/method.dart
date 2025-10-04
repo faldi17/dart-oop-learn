c3bf640d085d8a33aaa8adff23570963771e125f
Faldi <faldiansyahk@gmail.com>
2025-10-05 06:23:29 +0700

    add parameter to method road

--- bin/method.dart
+++ bin/method.dart
@@ -1,17 +1,17 @@
 void main() {
   Car car = Car();
-  print(car.road());
+  print(car.road(120, "Tol Serpong"));
   // car.road();
 }
 
 class Car {
-  String brand = "Unknow";
+  String brand = "Ferrari";
   final wheel = 4;
   String? color;
 
   // method road to call in main
-  String road() {
-    return "Ngeeeng... ngeeeng...";
+  String road(int speed, String location) {
+    return "Mobil $brand melaju dengan kecepatan $speed km/jam di $location. Ngeeeng... ngeeeng...";
   }
 
   // void road() {
